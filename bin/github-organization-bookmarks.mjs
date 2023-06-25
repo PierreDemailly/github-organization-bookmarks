24fbd997c5495f3db5c262c641b09bc2d5384c91
PierreDemailly <pierredemailly.pro@gmail.com>
2023-06-25 20:55:12 +0200

    refactor: use authorization header

--- bin/github-organization-bookmarks.mjs
+++ bin/github-organization-bookmarks.mjs
@@ -14,27 +14,24 @@ import pupa from "pupa";
 // CONSTANTS
 const kGitHubApiUrl = "https://api.github.com";
 const kTimestamp = Date.now();
+const kRequestOptions = {
+  headers: new Headers({
+    "X-GitHub-Api-Version": "2022-11-28"
+  }),
+  authorization: process.env.GITHUB_TOKEN
+};
+const kOrganization = process.argv[2];
 
-const orga = process.argv[2];
-if (!orga) {
+if (!kOrganization) {
   throw new Error("Missing organization name");
 }
 
-const headers = new Headers({
-  "X-GitHub-Api-Version": "2022-11-28"
-});
-const token = process.env.GITHUB_TOKEN;
-if (token) {
-  headers.set("Authorization", `Bearer ${token}`);
-}
-const { data: repos } = await request("GET",
-  new URL(`/users/${orga}/repos?per_page=100`, kGitHubApiUrl),
-  { headers }
-);
+const requestUrl = new URL(`/users/${kOrganization}/repos?per_page=100`, kGitHubApiUrl);
+const { data: repos } = await request("GET", requestUrl, kRequestOptions);
 
 const links = repos.map((repo) => `\t\t<DT><A HREF="${repo.html_url}" ADD_DATE="${kTimestamp}" LAST_MODIFIED="${kTimestamp}">${repo.name}</A>`).join(EOL);
 
 const template = fs.readFileSync(new URL("../template.html", import.meta.url), "utf-8");
-const bookmarks = pupa(template, { links, date: kTimestamp, orga });
+const bookmarks = pupa(template, { links, date: kTimestamp, orga: kOrganization });
 
-fs.writeFileSync(`./${orga}.html`, bookmarks);
+fs.writeFileSync(`./${kOrganization}.html`, bookmarks);
